b299cf592a9ae04ea0f16582275a3a1baf4cd32f
clar-reese <clar.reese@gmail.com>
2020-01-13 17:24:55 +0800

    Add example of usage

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,42 @@
-import React, { Component, Fragment } from 'react'
+import React, { useState } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { TableauExtension, TableauDialog, TableauDialogOpen } from 'tableau-extensions'
 
-import { TableauExtension } from 'tableau-extensions'
-
-export default class App extends Component {
-  render () {
-    return (
-      <Fragment>
-        <TableauExtension>This is a Tableau Extension</TableauExtension>
-      </Fragment>
-    )
-  }
+const Home = () => {
+  return (
+  	<button><TableauDialogOpen url="popup" dimensions={{height: 200, width: 300}}>
+	  	Click to view the list of worksheets!
+	  </TableauDialogOpen></button>
+	)
 }
+
+const Popup = () => {
+	const [worksheetList, setWorksheetList] = useState([]);
+
+	const dialogBuildFunction = () => {
+		const worksheets = window.tableau.extensions.dashboardContent.dashboard.worksheets;
+    setWorksheetList(worksheets.map(worksheet => worksheet.name));
+	}
+
+  return (
+  	<TableauDialog buildFunc={dialogBuildFunction}>
+	  	<h1>Worksheets:</h1>
+	  	<ul>{worksheetList.map(worksheet => <li>{worksheet}</li>)}</ul>
+  	</TableauDialog>
+  )
+}
+
+const Example = () => {
+  return (   
+    <TableauExtension> 
+      <BrowserRouter>
+        <Switch>
+           <Route path="/" component={Home} exact />
+           <Route path="/popup" component={Popup} />
+        </Switch>
+      </BrowserRouter>
+    </TableauExtension>
+  );
+}
+
+export default Example
